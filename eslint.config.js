d245580a4d8c06d114bdd6d69b0b1c69b62f0197
Nikita Smirnov <nikita.keyn@yandex.ru>
2025-08-15 09:16:21 +0300

    fix: update eslint configuration to add no-unused-vars rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,52 @@
 import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
+	baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
-    plugins: ['simple-import-sort'],
-    rules: {
-      'react/jsx-sort-props': [
-        'error',
-        {
-          reservedFirst: true,
-          multiline: 'last',
-        },
-      ],
-      'react/function-component-definition': [
-        'error',
-        {
-          namedComponents: 'arrow-function',
-          unnamedComponents: 'arrow-function',
-        },
-      ],
-      'react/display-name': [
-        'error',
-        {
-          ignoreTranspilerName: true,
-          checkContextObjects: true,
-        },
-      ],
-      'simple-import-sort/imports': [
-        'error',
-        {
-          groups: [
-            ['^react', '^next'],
-            ['^@?'],
-            ['^@/'],
-            ['^\\.\\./', '^\\./'],
-            ['\\.?(css|scss)$'],
-          ],
-        },
-      ],
-      'simple-import-sort/exports': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
-    },
-  }),
+	...compat.config({
+		extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+		plugins: ['simple-import-sort'],
+		rules: {
+			'react/jsx-sort-props': [
+				'error',
+				{
+					reservedFirst: true,
+					multiline: 'last',
+				},
+			],
+			'react/function-component-definition': [
+				'error',
+				{
+					namedComponents: 'arrow-function',
+					unnamedComponents: 'arrow-function',
+				},
+			],
+			'react/display-name': [
+				'error',
+				{
+					ignoreTranspilerName: true,
+					checkContextObjects: true,
+				},
+			],
+			'simple-import-sort/imports': [
+				'error',
+				{
+					groups: [
+						['^react', '^next'],
+						['^@?'],
+						['^@/'],
+						['^\\.\\./', '^\\./'],
+						['\\.?(css|scss)$'],
+					],
+				},
+			],
+			'simple-import-sort/exports': 'error',
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'@typescript-eslint/no-unused-vars': 'warn',
+		},
+	}),
 ];
 
 export default eslintConfig;
