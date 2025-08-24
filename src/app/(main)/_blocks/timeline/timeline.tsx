77779ad00f462f695fe07ab09bb16afaf7e822ac
push-ebx <danila.smirnov.07@list.ru>
2025-08-24 20:22:09 +0300

    refactor: update carousel controls and improve layout styles

--- src/app/(main)/_blocks/timeline/timeline.tsx
+++ src/app/(main)/_blocks/timeline/timeline.tsx
@@ -3,7 +3,7 @@ import { Body, Container, Description, Heading } from '@/components/ui';
 import styles from './timeline.module.scss';
 
 export const Timeline = () => {
-	const trackLineTitles = ['Первый трек', 'Второй трек'];
+	// const trackLineTitles = ['Первый трек', 'Второй трек'];
 
 	return (
 		<Container
@@ -18,9 +18,9 @@ export const Timeline = () => {
 			<div className={styles.trackLines}>
 				{trackLines.map((trackLine, i) => (
 					<div key={i} className={styles.trackLineWrapper}>
-						<Heading className={styles.trackLineTitle} color="violete" size="m">
-							{trackLineTitles[i]}
-						</Heading>
+						{/*<Heading className={styles.trackLineTitle} color="violete" size="m">*/}
+						{/*	{trackLineTitles[i]}*/}
+						{/*</Heading>*/}
 						<div className={styles.trackLine}>
 							{trackLine.map((track, j) => (
 								<div key={`${i}${j}`} className={styles.track}>
@@ -45,21 +45,21 @@ Timeline.displayName = 'Timeline';
 
 const trackLines = [
 	[
-		{ day: '22', month: 'августа', title: 'Завершение\nрегистрации' },
-		{ day: '27', month: 'сентября', title: 'Открытие\nпрограммы' },
-		{ day: '01', month: 'октября', title: 'Обучающие\nблоки и трекинг' },
-		{ day: '08', month: 'ноября', title: 'Пицца питч' },
-		{ day: '29', month: 'ноября', title: 'Демодей' },
-		{ day: '26', month: 'декабря', title: 'Хакатон' },
-		{ day: '06', month: 'января', title: 'Пост-поддержка' },
+		{ day: '22', month: 'сентября', title: 'Завершение\nрегистрации' },
+		{ day: '25', month: 'сентября', title: 'Открытие\nпрограммы' },
+		{ day: '27', month: 'сентября', title: 'Обучающие\nблоки и трекинг' },
+		{ day: '31', month: 'октября', title: 'Хакатон' },
+		{ day: '24', month: 'ноября', title: 'Пицца питч' },
+		{ day: '06', month: 'декабря', title: 'Демодей' },
+		{ day: '12', month: 'декабря', title: 'Пост-поддержка' },
 	],
-	[
-		{ day: '22', month: 'августа', title: 'Завершение\nрегистрации' },
-		{ day: '27', month: 'сентября', title: 'Открытие\nпрограммы' },
-		{ day: '01', month: 'октября', title: 'Обучающие\nблоки и трекинг' },
-		{ day: '08', month: 'ноября', title: 'Пицца питч' },
-		{ day: '29', month: 'ноября', title: 'Демодей' },
-		{ day: '26', month: 'декабря', title: 'Хакатон' },
-		{ day: '06', month: 'января', title: 'Пост-поддержка' },
-	],
+	// [
+	// 	{ day: '22', month: 'августа', title: 'Завершение\nрегистрации' },
+	// 	{ day: '27', month: 'сентября', title: 'Открытие\nпрограммы' },
+	// 	{ day: '01', month: 'октября', title: 'Обучающие\nблоки и трекинг' },
+	// 	{ day: '08', month: 'ноября', title: 'Пицца питч' },
+	// 	{ day: '29', month: 'ноября', title: 'Демодей' },
+	// 	{ day: '26', month: 'декабря', title: 'Хакатон' },
+	// 	{ day: '06', month: 'января', title: 'Пост-поддержка' },
+	// ],
 ];
--- src/hooks/client/use-carousel-controls.ts
+++ src/hooks/client/use-carousel-controls.ts
@@ -1,15 +1,15 @@
 import { useCallback, useState } from 'react';
 
-export const useCarouselControls = (length: number, visibleSlides = 3) => {
+export const useCarouselControls = (length: number, visibleSlides = 1) => {
 	const [activeSlide, setActiveSlide] = useState(0);
 
 	const handleNext = useCallback(() => {
-		setActiveSlide((prev) => Math.min(prev + 1, length - visibleSlides));
-	}, [length, visibleSlides]);
+		setActiveSlide((prev) => (prev + 1) % length);
+	}, [length]);
 
 	const handlePrev = useCallback(() => {
-		setActiveSlide((prev) => Math.max(prev - 1, 0));
-	}, []);
+		setActiveSlide((prev) => (prev - 1 + length) % length);
+	}, [length]);
 
 	return { activeSlide, handleNext, handlePrev, setActiveSlide };
 };
