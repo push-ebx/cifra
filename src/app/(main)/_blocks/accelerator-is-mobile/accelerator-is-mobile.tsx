63ccb454dae3ae1c952649f7c41590ec346e5665
Nikita Smirnov <nikita.keyn@yandex.ru>
2025-09-10 17:56:27 +0300

    cifra this carousel

--- src/app/(main)/_blocks/accelerator-is-mobile/accelerator-is-mobile.tsx
+++ src/app/(main)/_blocks/accelerator-is-mobile/accelerator-is-mobile.tsx
@@ -1,9 +1,18 @@
+'use client';
+
+import { clsx } from 'clsx';
+
 import { backpack, gold, grand, ok } from '@/components/icons';
-import { Body, Container, Heading } from '@/components/ui';
+import { Body, Container, Description, Heading } from '@/components/ui';
+import { Carousel } from '@/components/ui/carousel/carousel';
+import { CarouselDots } from '@/components/ui/carousel-dots/carousel-dots';
+import { useCarouselControls } from '@/hooks/client/use-carousel-controls';
 
 import styles from './accelerator-is-mobile.module.scss';
 
 export const AcceleratorIsMobile = () => {
+	const { activeSlide, setActiveSlide } = useCarouselControls(1);
+
 	return (
 		<Container className={styles.root} data-theme="white" tag="section">
 			<Heading className={styles.heading} color="violete" size="xl">
@@ -11,17 +20,45 @@ export const AcceleratorIsMobile = () => {
 			</Heading>
 			<div className={styles.wrapper}>
 				<div className={styles.cards}>
-					{cards.map((card, index) => (
-						<div key={index} className={styles.card}>
-							<div className={styles.cardText}>
-								<Heading size="m">{card.title}</Heading>
-								<Body color="darkGray" size="s">
-									{card.subtitle}
-								</Body>
+					<Carousel
+						active={activeSlide}
+						className={styles.carousel}
+						onChangeAction={(index) => setActiveSlide(index)}
+						onClick={(e) => e.stopPropagation()}
+						options={{
+							align: 'center',
+							dragFree: false,
+							containScroll: 'trimSnaps',
+							skipSnaps: true,
+							loop: false,
+						}}
+					>
+						{cards.map((card, index) => (
+							<div
+								key={index}
+								className={clsx(styles.card, styles[`card-${index + 1}`])}
+							>
+								<Description className={styles.index}>0{index + 1}</Description>
+								<div className={styles.cardText}>
+									<Heading className={styles.title} size="m">
+										{card.title}
+									</Heading>
+									<Body color="darkGray" size="s">
+										{card.subtitle}
+									</Body>
+								</div>
+								{/*<span className={styles.icon}>{card.icon}</span>*/}
 							</div>
-							<span className={styles.icon}>{card.icon}</span>
-						</div>
-					))}
+						))}
+					</Carousel>
+					<div className={styles.carouselDots}>
+						<CarouselDots
+							activeColor="#752CE8"
+							activeSlide={activeSlide}
+							onDotClick={setActiveSlide}
+							totalSlides={cards?.length ?? 0}
+						/>
+					</div>
 				</div>
 			</div>
 		</Container>
@@ -32,22 +69,22 @@ AcceleratorIsMobile.displayName = 'AcceleratorIsMobile';
 
 const cards = [
 	{
-		title: '#Студенты',
+		title: 'Студенты',
 		subtitle: 'Запуск стартапа с нуля для студентов',
 		icon: backpack,
 	},
 	{
-		title: '#Единомышленники',
+		title: 'Единомышленники',
 		subtitle: 'IT-комьюнити, обмен опытом и сеть профессиональных контактов',
 		icon: ok,
 	},
 	{
-		title: '#Инвестиции',
+		title: 'Инвестиции',
 		subtitle: 'Шанс привлечь инвестиции от венчурных инвесторов',
 		icon: gold,
 	},
 	{
-		title: '#Кураторы',
+		title: 'Кураторы',
 		subtitle: 'Ведущие предприниматели, инвесторы и преподаватели вузов России',
 		icon: grand,
 	},
