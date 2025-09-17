a88258bd83614cf5c9b14d145181700a4952008d
push-ebx <danila.smirnov.07@list.ru>
2025-09-17 10:48:02 +0300

    feat: seo files

---
+++ src/app/sitemap.ts
@@ -0,0 +1,10 @@
+export default async function sitemap() {
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
