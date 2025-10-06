e0c4ee2a6edb66c298ec31f435b5c35d9b346647
ma1oi <danila.smirnov.07@list.ru>
2025-10-06 19:39:22 +0300

    partners

--- src/app/(main)/_blocks/partners/partners.tsx
+++ src/app/(main)/_blocks/partners/partners.tsx
@@ -142,6 +142,14 @@ const mobilePartners = [
 		url: 'https://hh.ru/',
 	},
 	{
+		src: '/images/partners/pik.webp',
+		url: 'https://pecom.ru/',
+	},
+	{
+		src: '/images/partners/kmos.webp',
+		url: 'https://www.mos.ru/glavkontrol',
+	},
+	{
 		src: '/images/partners/fsp.webp',
 		url: 'https://fsp-russia.ru/',
 	},
